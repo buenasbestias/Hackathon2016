5d76ee93eaddd5b15f18b03a4384ee8b2ae6bee5
Jakob H. Renpening <jakobhans@gmail.com>
2016-02-20 21:19:42 -0600

    Testing JSON format

--- ajax.php
+++ ajax.php
@@ -8,14 +8,14 @@ include_once('.env');
 
 $bpm = $_GET['bpm'];
 
-$ch = curl_init('http://developer.echonest.com/api/v4/song/search?format=xml&api_key=' . ECHONEST_API . '&max_tempo=' . $bpm . '&min_tempo=' . $bpm);
+$ch = curl_init('http://developer.echonest.com/api/v4/song/search?api_key=' . ECHONEST_API . '&max_tempo=' . $bpm . '&min_tempo=' . $bpm);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 $data = curl_exec($ch);
 curl_close($ch);
 
 echo "<pre>";
-var_dump($data);
+var_dump(json_decode($data));
 echo "</pre>";
 
 ?>
