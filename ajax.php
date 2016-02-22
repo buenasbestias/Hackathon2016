f7f24f8a4c022960240505f1de5bbe18a334b564
Jakob H. Renpening <jakobhans@gmail.com>
2016-02-21 22:30:53 -0600

    Testing some other way to simplify data structure

--- ajax.php
+++ ajax.php
@@ -11,11 +11,11 @@ $bpm = $_GET['bpm'];
 $ch_echonest = curl_init('http://developer.echonest.com/api/v4/song/search?api_key=' . ECHONEST_API . '&max_tempo=' . $bpm . '&min_tempo=' . $bpm);
 curl_setopt($ch_echonest, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch_echonest, CURLOPT_HEADER, 0);
-$data['echonest'] = json_decode(curl_exec($ch_echonest));
+$echonest = json_decode(curl_exec($ch_echonest));
 curl_close($ch_echonest);
 
-$song = $data['echonest']->response->songs[0];
-$query = urlencode($song->artist_name . ' ' . $song->title);
+$data['echonest'] = $echonest->response->songs[0];
+$query = urlencode($song->artist_name . ' ' . $data['echonest']->title);
 
 $ch_spotify = curl_init('https://api.spotify.com/v1/search?q=' . $query . '&type=track&limit=1');
 curl_setopt($ch_spotify, CURLOPT_RETURNTRANSFER, true);
@@ -23,7 +23,9 @@ curl_setopt($ch_spotify, CURLOPT_HEADER, 0);
 $spotify = json_decode(curl_exec($ch_spotify));
 curl_close($ch_spotify);
 
-$data['spotify_link'] = $spotify->tracks->items[0]->external_urls->spotify;
+if (isset($spotify->tracks->items[0])) {
+    $data['spotify_link'] = $spotify->tracks->items[0]->external_urls->spotify;
+}
 
 echo "<pre>";
 echo "<h1>EchoNest</h1>";
